74ca5aa4245a84436efc46f30fd32f9946bd2499
Logoseros Das <dlogoseros@gmail.com>
2021-12-28 13:10:06 +0530

    28/12 main.js

--- main.js
+++ main.js
@@ -5,55 +5,82 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector('#c1')
-});
-
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-camera.position.setZ(30);
-
-//to load your object .glb format
-const loader = new GLTFLoader();
-loader.load('basic_model_3js.glb', function(glb) {
-  const root = glb.scene;
-  scene.add(root);
-})
+//canvas
+const canvas = document.querySelector('#c1')
+//scene
+const scene = new THREE.Scene()
 
 
+//group1
+const group1 = new THREE.Group()
 
-//geometry mesh material
-//*const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-//instead of || const material = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
-// use MeshStandardMaterial for light reflection
-//*const material = new THREE.MeshStandardMaterial( { color: 0xabff00, } );
-//*const torus = new THREE.Mesh( geometry, material );
-//*scene.add( torus );
+scene.add(group1)
 
-//point-light
-const pointLight = new THREE.PointLight(0xfff4d);
-// for torus  * pointLight.position.set(10,7,11);
+//geometries that go into group
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const material = new THREE.MeshStandardMaterial({ color: 0xcd2323 })
+
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshStandardMaterial({ color: 0xcd2323 })
+)
+group1.add(mesh)
+
+
+// ### group modification
+group1.position.set(1.2,1,1)
+
+group1.scale.set(1,1.2,2)
+
+group1.rotation.reorder('XYZ')
+group1.rotation.x = 0.06
+
+
+
+// ### length vector of object wrt origin
+// console.log(mesh.position.length())
+
+// ### normalize reduces the distance vector wrt origin and makes it 1
+//mesh.position.normalize()
+
+
+//sizes variables
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+// camera.position.z = 3
+camera.position.set(0,0,5)
+
+scene.add(camera)
+
+camera.lookAt(group1.position)
+
+// ### distance of object from camera
+// console.log(mesh.position.distanceTo(camera.position))
+
+//light
+//not needed for MeshBasicMaterial
+const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(-5,5,3);
 
-//ammbient-light
-const ambientLight = new THREE.AmbientLight(0xfcf2dc);
-
-scene.add(pointLight, ambientLight);
-
-//helpers
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200,30);
-scene.add(lightHelper, gridHelper);
+scene.add(pointLight)
 
 
-//orbit controls with cursor
-//don't forget to (1)import and (2)add << controls.update() >> 
-  //inside rendering function below
+//helper
+const gridHelper = new THREE.GridHelper()
+scene.add(gridHelper)
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+//orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
@@ -61,9 +88,9 @@ const controls = new OrbitControls(camera, renderer.domElement);
 function animate() {
   requestAnimationFrame(animate);
 
-  // torus.rotation.x += 0.01;
-  // torus.rotation.y += 0.01;
-  // torus.rotation.z += 0.005;
+  group1.rotation.x += 0.01;
+  group1.rotation.y += 0.01;
+  group1.rotation.z += 0.005;
 
   controls.update();
 
